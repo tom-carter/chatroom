cb223c0cc870cde0087812450af676be977d9e41
Tom Carter <tom.carter@u2i.com>
2022-07-09 18:32:21 +0200

    Adds styling for devise

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
   def create
     @current_user = current_user
     @message = @current_user.messages.create(body: message_params[:body])
